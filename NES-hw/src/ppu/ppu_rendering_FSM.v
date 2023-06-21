e3a27477c75baacbe2a9c8392055a6664126e591
Dominik Varga <ssominhun@gmail.com>
2023-06-21 12:01:24 +0200

    Fixing state borders in BG rendering

--- NES-hw/src/ppu/ppu_rendering_FSM.v
+++ NES-hw/src/ppu/ppu_rendering_FSM.v
@@ -72,13 +72,13 @@ parameter BG_Lsb = 3'b100;
 parameter BG_Msb = 3'b101;
 parameter VBLANK = 3'b110;
 
-parameter start_rendering_line = 11'd123;
-parameter start_of_last_NT = 11'd1478;
-parameter end_of_BG_renderingline = 11'd1486;
-parameter bg_next_step_condition = 3'b111;
+parameter start_rendering_line = 11'd127; 
+parameter start_of_last_NT = 11'd1482; 
+parameter end_of_BG_renderingline = 11'd1490;
+parameter bg_next_step_condition = 3'b011;
 
-parameter oddframe_end_of_first_NT = 11'd127;
-parameter oddframe_end_of_BG_renderingline = 11'd1482;
+parameter oddframe_end_of_first_NT = 11'd131;
+parameter oddframe_end_of_BG_renderingline = 11'd1486;
 
 parameter end_of_visible_frame_row = 9'd239;
 parameter end_of_VBLANK_row = 9'd260;
@@ -94,7 +94,7 @@ begin
 		bgrender_state <= next_state;
 end
 
-//PPU BG rendering state machine
+//PPU BG rendering state machine with oddframe changes
 always @ (*)
 begin
 	case (bgrender_state)
@@ -148,6 +148,55 @@ begin
 				next_state <= SLEEP;
 			else
 				next_state <= VBLANK;
+		end
+		default:
+			next_state <= 3'bxxx;
+	endcase
+end
+
+// BG rendering without oddframe counting withou VBLANK
+always @ (*)
+begin
+	case (bgrender_state)
+		SLEEP: begin
+			if (x_rendercntr == start_rendering_line && ((y_renderingcntr == prerendering_row) || (y_renderingcntr <= end_of_visible_frame_row)))
+				next_state <= IDLE;
+			else
+				next_state <= SLEEP;
+		end
+		IDLE: begin
+			if (x_rendercntr[2:0] == bg_next_step_condition)
+				next_state <= NT;
+			else
+				next_state <= IDLE;
+		end
+		NT: begin
+			if (x_rendercntr == end_of_BG_renderingline)
+				next_state <= SLEEP;
+			else if (x_rendercntr == start_of_last_NT)
+				next_state <= NT;
+			else if (x_rendercntr[2:0] == bg_next_step_condition)
+				next_state <= AT;
+			else
+				next_state <= NT;
+		end
+		AT: begin
+			if (x_rendercntr[2:0] == bg_next_step_condition)
+				next_state <= BG_Lsb;
+			else
+				next_state <= AT;
+		end
+		BG_Lsb: begin
+			if (x_rendercntr[2:0] == bg_next_step_condition)
+				next_state <= BG_Msb;
+			else
+				next_state <= BG_Lsb;
+		end
+		BG_Msb: begin
+			if (x_rendercntr[2:0] == bg_next_step_condition)
+				next_state <= NT;
+			else
+				next_state <= BG_Msb;
 		end
 		default:
 			next_state <= 3'bxxx;
